480cc5bb64588e8ee292adf591add1e97c948b2b
Amar Fazlić <amar.fazlic@stu.ibu.edu.ba>
2022-12-28 18:04:23 +0100

    Initial commit

---
+++ Labs-SVVT/src/main/java/ba/edu/ibu/App.java
@@ -0,0 +1,7 @@
+package ba.edu.ibu;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ Labs-SVVT/src/test/java/ba/edu/ibu/AppTest.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
